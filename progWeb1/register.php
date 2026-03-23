454cd702f75d952d8d5b91be83a82546a98441cb
lucasdias69 <lucasvbdias@gmail.com>
2026-03-23 11:25:15 -0300

    Update and rename register.html to register.php

--- progWeb1/register.php
+++ progWeb1/register.php
@@ -4,28 +4,34 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Criar Conta - Carteira Acadêmica</title>
-<link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
   <main class="container">
     <h2>Criar conta</h2>
-    <form action="#" method="post" class="form">
+
+    <form action="auth.php" method="post" class="form">
+      <input type="hidden" name="acao" value="register">
+
       <label>Nome completo
         <input type="text" name="fullname" required />
       </label>
+
       <label>Email
         <input type="email" name="email" required />
       </label>
+
       <label>Senha
         <input type="password" name="password" required />
       </label>
+
       <button class="btn" type="submit">Criar conta</button>
     </form>
 
     <p class="small">
-      Já tem conta? <a href="login.html">Entrar</a>
+      Já tem conta? <a href="login.php">Entrar</a>
     </p>
-    <p><a href="index.html">← Voltar</a></p>
+    <p><a href="index.php">← Voltar</a></p>
   </main>
 </body>
 </html>
