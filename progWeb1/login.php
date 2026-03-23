6e2efb26cf09daf50e338dcc069ad544c002fc63
lucasdias69 <lucasvbdias@gmail.com>
2026-03-23 11:24:38 -0300

    Update and rename login.html to login.php

--- progWeb1/login.php
+++ progWeb1/login.php
@@ -4,25 +4,30 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Login - Carteira Acadêmica</title>
-  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
   <main class="container">
     <h2>Entrar</h2>
-    <form action="#" method="post" class="form">
-      <label>Usuário
+
+    <form action="auth.php" method="post" class="form">
+      <input type="hidden" name="acao" value="login">
+
+      <label>Usuário (Email)
         <input type="text" name="username" required />
       </label>
+
       <label>Senha
         <input type="password" name="password" required />
       </label>
+
       <button class="btn" type="submit">Entrar</button>
     </form>
 
     <p class="small">
-      Não tem login? <a href="register.html">Faça agora</a>
+      Não tem login? <a href="register.php">Faça agora</a>
     </p>
-    <p><a href="index.html">← Voltar</a></p>
+    <p><a href="index.php">← Voltar</a></p>
   </main>
 </body>
 </html>
